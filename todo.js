aa633266c47bdfc0783f5b3cd7ecda9f9254f8bb
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2020-12-18 10:56:29 +0530

    updated more functionality

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 var tasks=0;
+var toggle=0;
 console.log("Aditya's ToDo List");
 var input = document.getElementById('add');
 input.addEventListener("keyup", event => {
@@ -9,21 +10,54 @@ input.addEventListener("keyup", event => {
 function addTask(){
     if(document.getElementById('add').value!=''){
         tasks++;
+       
         var task = document.createElement("div");
-        task.innerHTML= document.getElementById('add').value;
-        task.id='task';
+        task.innerHTML= tasks+". "+document.getElementById('add').value;
+        task.className='task';
+        task.id=('task'+tasks).toString();
+        var tas=task.id;
+        
+        var d = new Date();
+        var date = document.createElement('span');
+        date.className='date';
+        date.innerHTML = d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear();
+        date.style.fontSize='15px';
+
+        
         var remove = document.createElement('button')
         remove.innerHTML='&#128077;';
-        remove.id='remove';
+        remove.className='remove';
+        remove.id=('remove'+tasks).toString();
+        var rem=remove.id;
+        
         var edit = document.createElement('button');
         edit.innerHTML = '&#128393;';
-        edit.id='edit';
+        edit.className='edit';
+        edit.id=('edit'+tasks).toString();
+        var edi=edit.id;
+        
         task.appendChild(remove);
         task.appendChild(edit);
+        task.appendChild(date);
+
         document.getElementById('add').value='';
         document.getElementById('tasks').appendChild(task);
+    
+
     }
     else{
         console.log("Aditya's ToDo List")
+    }
+}
+function setImportance(){
+    if(toggle===0){
+        document.getElementById('imp').innerHTML=String.fromCharCode(9733);
+        document.getElementById('imp').style.color='rgb(230, 184, 0)';
+        toggle=1;
+    }
+    else{
+        document.getElementById('imp').innerHTML=String.fromCharCode(9734);
+        document.getElementById('imp').style.color='rgb(0,0,0)';
+        toggle=0;
     }
 }
